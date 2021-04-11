b8b93a75c2c04045c07af70673695eaf75d96493
hermann tamouya <hermann12@gmail.com>
2021-04-11 13:21:15 +0200

    fix the port

--- controllers/deleteItem.js
+++ controllers/deleteItem.js
@@ -2,7 +2,7 @@ const Shop = require("../models/connection")
 const deleteItem = (req, res) => {
     const id = req.params.id
     Shop.findOneAndRemove({ _id: id })
-        .then(() => res.status(200).send())
+        .then(() => res.status(200).send(""))
         .catch(e => res.status(500).json({ message: e.message }))
 }
 
--- controllers/isItemDone.js
+++ controllers/isItemDone.js
@@ -2,7 +2,7 @@ const Shop = require("../models/connection")
 const isItemDone = (req, res) => {
     const id = req.params.id
     Shop.findOneAndUpdate({ _id: id }, { isDone: true })
-        .then(() => console.log("done!"))
+        .then(() => res.send(""))
         .catch(e => console.log(e.message))
 }
 
